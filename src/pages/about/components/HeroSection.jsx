6df4fb0339d51b1046ac25e3e07780ae49f3da9a
Ankit Dhiman <ankitdhiman@Ankits-MacBook-Air.local>
2025-08-24 20:24:00 +0530

    image added

--- src/pages/about/components/HeroSection.jsx
+++ src/pages/about/components/HeroSection.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import logo from "../../../assets/images/Stalwart_Logo.png"
+import React from "react";
+import logo from "../../../assets/images/Stalwart_Logo.png";
+import bgImage from "../../../assets/images/bg-image.png"; // import background
 
 const HeroSection = () => {
   return (
     <section
       className="h-screen flex items-center justify-center bg-cover bg-center px-12.5 md:px-0"
       style={{
-        backgroundImage: "url('/src/assets/images/bg-image.png')",
+        backgroundImage: `url(${bgImage})`, // use imported image
       }}
     >
-      <div className='container'>
-        <div className='flex items-center justify-between'>
-            <div><img src={logo} alt="logo"  /></div>
-            <div className='text-white uppercase'>ABOUT US</div>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="text-white uppercase">ABOUT US</div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
