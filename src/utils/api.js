a9d385a07470e86cc94fc31c3488a7def68f1a7d
Ankit Dhiman <ankitdhiman@Ankits-MacBook-Air.local>
2025-10-14 16:23:13 +0530

    serve on ngrok

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " https://e7a133872ebf.ngrok-free.app", // GraphQL endpoint
-  // baseURL: import.meta.env.VITE_BASE_URL, // GraphQL endpoint
+  // baseURL: " https://e7a133872ebf.ngrok-free.app", // GraphQL endpoint
+  baseURL: import.meta.env.VITE_BASE_URL, // GraphQL endpoint
   headers: {
     "Content-Type": "application/json",
   },
