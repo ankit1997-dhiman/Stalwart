4371e372cf111b38191e22c93216518182729e18
Ankit Dhiman <ankitdhiman@Ankits-MacBook-Air.local>
2025-10-17 14:59:16 +0530

    here is the latest branch

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 console.log(import.meta.env.VITE_BASE_URL, "base url");
 const api = axios.create({
-<<<<<<< HEAD
-  // baseURL: "http://72.60.76.204/api/graphql", // GraphQL endpoint
-=======
-  // baseURL: "http://localhost:3002/api/graphql", 
->>>>>>> b736b2ebd487e1e7b5efd582db874f665d221b9d
+  // baseURL: "http://localhost:3002/api/graphql",
   baseURL: import.meta.env.VITE_BASE_URL, // GraphQL endpoint
   headers: {
     "Content-Type": "application/json",
