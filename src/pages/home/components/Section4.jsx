789f73a52bae37cf78a56d8c2bca5d50163dc672
ankit <shwaikankit@gmail.com>
2025-11-05 15:15:53 +0530

    chnages on home page

--- src/pages/home/components/Section4.jsx
+++ src/pages/home/components/Section4.jsx
@@ -6,23 +6,19 @@ import { HoverSection } from "./HoverSection.jsx";
 
 export const Section4 = () => {
   return (
-    <section className="w-full flex lg:flex-row flex-col items-stretch justify-between lg:gap-7.5 h-[400px] md:h-[1080px] md:py-9">
-      <div className="xl:hidden block flex-1 bg-cover bg-center transition-all duration-700">
-        <img src={mobileImage1} />
-      </div>
-
+    <section className="w-full flex lg:flex-row flex-col items-stretch justify-between lg:gap-7.5 h-full lg:h-[1080px] md:py-9">
       <div
-        className="xl:block hidden w-full lg:w-[30%] relative group overflow-hidden lg:h-full bg-cover bg-center transition-opacity duration-700 "
+        className="lg:block hidden w-full lg:w-[30%] relative group overflow-hidden lg:h-full bg-cover bg-center transition-opacity duration-700 "
         style={{ backgroundImage: `url(${image1})` }}
       >
         <HoverSection />
       </div>
 
-      <div className=" xl:flex w-full  lg:w-[70%]  flex-col gap-y-9 h-full">
+      <div className=" lg:flex w-full  lg:w-[70%]  flex-col gap-y-9 h-full">
         <div className="flex-1 relative group overflow-hidden bg-black transition-all duration-700">
           <video
             src={video1}
-            className=" lg:absolute inset-0 w-full lg:h-full object-cover h-[300px]"
+            className=" lg:absolute inset-0 w-full lg:h-full object-cover "
             autoPlay
             muted
             loop
